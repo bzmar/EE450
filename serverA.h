e7d76737e8eaedff2af1bbbf3253def09349c05c
Brian Mar <Brianmar96@gmail.com>
2024-11-03 20:25:41 -0800

    fix includes in serverA header

--- serverA.h
+++ serverA.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <iostream>
+#include <thread>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 const int MY_ID_NUMBER_LAST_THREE_DIGITS = 209;
 const int UDP_PORT = 21000 + MY_ID_NUMBER_LAST_THREE_DIGITS;
